056ede60bfe5063f839918d9e6ff2e3f834218ad
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-05-31 12:35:35 +0200

    Move constants into their own package

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,7 +1,8 @@
-package types
+package constants
 
 import (
 	"math"
+	"time"
 )
 
 // CurrencyName is the official name of the currency
@@ -18,3 +19,18 @@ const QuantaPerUnit = 100000000
 // MaxQuantaPerAddress is the number of quanta that
 // can be tracked in a single address
 const MaxQuantaPerAddress = math.MaxInt64
+
+// Epoch is the basic moment from which Ndau chain time calculations begin
+var Epoch time.Time
+
+func init() {
+	tz, err := time.LoadLocation("Europe/Berlin")
+	if err != nil {
+		panic(err)
+	}
+	Epoch = time.Date(
+		2018, time.January, 18,
+		14, 21, 0,
+		0, tz,
+	)
+}
