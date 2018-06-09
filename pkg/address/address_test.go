8610297f8a4c34407543b3359b0631005de45a24
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-06-09 18:31:35 +0200

    Add test and benchmark of address generation

---
+++ pkg/address/address_test.go
@@ -0,0 +1,55 @@
+package address
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func getKinds() []Kind {
+	return []Kind{
+		KindUser,
+		KindNdau,
+		KindExchange,
+		KindEndowment,
+	}
+}
+
+func TestArbitraryAddressesAreValid(t *testing.T) {
+	kinds := getKinds()
+	for i := 0; i < 16; i++ {
+		key := make([]byte, 32)
+		_, err := rand.Read(key)
+		require.NoError(t, err)
+
+		t.Run(string(i), func(t *testing.T) {
+			t.Log("key", fmt.Sprintf("%x", key))
+			address, err := Generate(kinds[i&3], key)
+			require.NoError(t, err)
+			t.Log("address", address)
+			require.NoError(t, Validate(address))
+		})
+	}
+}
+
+func BenchmarkGeneration(b *testing.B) {
+	key := make([]byte, 32)
+	kinds := getKinds()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		_, err := rand.Read(key)
+		if err != nil {
+			b.FailNow()
+		}
+		b.StartTimer()
+
+		_, err = Generate(kinds[n&3], key)
+		if err != nil {
+			b.FailNow()
+		}
+	}
+}
