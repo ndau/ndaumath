957e6fedd3510c5df631c5efb918797cf17dd0f6
Kent Quirk <kentquirk@gmail.com>
2018-07-05 13:51:31 -0400

    Run the limited test we have

--- cmd/keyaddr/keyaddr_test.js
+++ cmd/keyaddr/keyaddr_test.js
@@ -6,7 +6,10 @@ mk.then((k) => {
     console.log(k)
     pubkey = k.Neuter()
     console.log(pubkey);
-}).catch((e) => console.log("error: ", e));
+}).catch((e) => {
+    console.log("master key error: ", e);
+    process.exit(1);
+});
 
 mk.then((k) => {
     msg = "CAFEF00DBAAD1DEA"
@@ -15,7 +18,17 @@ mk.then((k) => {
         pubkey = k.Neuter()
         sig.Verify(msg, k.key).then((b) => {
             console.log("Verified: ", b)
-        }).catch((e) => console.log("No verify!", e));
-    }).catch((e) => console.log("sigerror: ", e));
+        }).catch((e) => {
+            console.log("NO verify: ", e);
+            process.exit(1);
+        });
 
-}).catch((e) => console.log("error: ", e));
+    }).catch((e) => {
+        console.log("sigerror: ", e);
+        process.exit(1);
+    });
+
+}).catch((e) => {
+    console.log("error: ", e);
+    process.exit(1);
+});
