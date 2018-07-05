000ccc311d93f301977c2bb79a2e422abdf786ee
Kent Quirk <kentquirk@gmail.com>
2018-07-05 18:52:10 -0400

    Add lots of comments re tests, plus a couple of test cases

--- cmd/keyaddr/words_test.go
+++ cmd/keyaddr/words_test.go
@@ -67,9 +67,13 @@ func TestPadData(t *testing.T) {
 		want  int
 		want1 []byte
 	}{
+		// because of the way our CRC works, input of 0 will have a CRC of 0
 		{"a", args{[]byte{0x00, 0x00}}, 2, []byte{0x00, 0x00, 0x00}},
+		// but change one bit and the crc changes
 		{"b", args{[]byte{0x80, 0x00}}, 2, []byte{0x80, 0x00, 36}},
+		// here's a more normal short one
 		{"c", args{[]byte{0x82, 0x41}}, 2, []byte{0x82, 0x41, 124}},
+		// and this is the basic 16-byte test; it should generate a 12-word result
 		{"d", args{[]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}}, 12,
 			[]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 32}},
 	}
@@ -97,16 +101,22 @@ func TestWordsFromBytes(t *testing.T) {
 		want    []string
 		wantErr bool
 	}{
+		// a simple input yields a simple output
 		{"a", args{"en", []byte{0, 1, 2}}, []string{"abandon", "amount", "mom"}, false},
+		// this is the generic basic test
 		{"b", args{"en", []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}},
 			strings.Split("abandon amount liar amount expire adjust cage candy arch gather drum bundle", " "), false},
+		// if you change the first byte, the last and the first word should change
 		{"c", args{"en", []byte{100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}},
 			strings.Split("goat amount liar amount expire adjust cage candy arch gather drum buyer", " "), false},
+		// these next items were failures identified early in fuzz testing
 		{"d", args{"en", []byte{15, 218, 104}}, strings.Split("average spy bicycle", " "), false},
 		{"e", args{"en", []byte{91, 165, 36, 247, 53, 142, 251, 181, 184, 50, 32, 207, 88, 99, 108, 188, 64, 207, 172, 154, 235, 60, 200, 192}},
 			strings.Split("forum circle differ help use suspect this dune soon seek swamp joy artefact stone hill guide silver addict", " "), false},
 		{"f", args{"en", []byte{41, 247, 253, 146, 141, 146, 202, 67, 241, 147, 222, 228, 127, 89, 21, 73, 245, 151, 168, 17, 200}},
 			strings.Split("clarify say gorilla brass coach capable shock knock tongue width earn negative floor staff elbow aim", " "), false},
+		// check that a bad language is detected
+		{"g", args{"sp", []byte{0, 1, 2}}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -133,6 +143,7 @@ func TestBytesFromWords(t *testing.T) {
 		want    []byte
 		wantErr bool
 	}{
+		// these are the reverse of all of the tests above
 		{"a", args{"en", []string{"abandon", "amount", "mom"}}, []byte{0, 1, 2}, false},
 		{"b", args{"en", strings.Split("abandon amount liar amount expire adjust cage candy arch gather drum bundle", " ")},
 			[]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, false},
@@ -145,6 +156,8 @@ func TestBytesFromWords(t *testing.T) {
 		{"f", args{"en", strings.Split("clarify say gorilla brass coach capable shock knock tongue width earn negative floor staff elbow aim", " ")},
 			[]byte{41, 247, 253, 146, 141, 146, 202, 67, 241, 147, 222, 228, 127, 89, 21, 73, 245, 151, 168, 17, 200},
 			false},
+		// check for bad language
+		{"g", args{"sp", []string{"abandon", "amount", "mom"}}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -160,6 +173,9 @@ func TestBytesFromWords(t *testing.T) {
 	}
 }
 
+// This does a randomized test of the roundtrip -- it generates random lengths (1-32) of random data bytes
+// then converts them to words and back to data; do this 10K times and we're reasonably confident that
+// the algorithm works properly.
 func Test_RoundTrip(t *testing.T) {
 	for i := 0; i < 10000; i++ {
 		nbytes := rand.Intn(32) + 1
@@ -192,6 +208,8 @@ func Test_lookupWord(t *testing.T) {
 		want    int
 		wantErr bool
 	}{
+		// test the first, second, and last words, plus one in the niddle, plus
+		// one that's not there
 		{"a", args{"en", "abandon"}, 0, false},
 		{"b", args{"en", "ability"}, 1, false},
 		{"c", args{"en", "roof"}, 1501, false},
@@ -223,9 +241,13 @@ func Test_setBit(t *testing.T) {
 		args args
 		want []byte
 	}{
-		{"a", args{3, []byte{0}, 1}, []byte{0x10}},
-		{"b", args{13, []byte{0, 0}, 1}, []byte{0x00, 0x04}},
-		{"c", args{13, []byte{0xFF, 0xFF}, 0}, []byte{0xFF, 0xFB}},
+		// make sure we can set arbitrary bits in an array of bytes
+		{"a", args{0, []byte{0}, 1}, []byte{0x80}},
+		{"b", args{7, []byte{0}, 1}, []byte{0x01}},
+		{"c", args{3, []byte{0}, 1}, []byte{0x10}},
+		{"d", args{13, []byte{0, 0}, 1}, []byte{0x00, 0x04}},
+		// and also clear them
+		{"e", args{13, []byte{0xFF, 0xFF}, 0}, []byte{0xFF, 0xFB}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -248,9 +270,17 @@ func Test_setRun(t *testing.T) {
 		args args
 		want []byte
 	}{
+		// set an arbitrary chunk of 11 bits to 1s and 0s.
 		{"a", args{1, 11, []byte{0, 0, 0, 0}, 0x55}, []byte{0, 0x01, 0x54, 0}},
+		// set 11 1s in a row
 		{"b", args{1, 11, []byte{0, 0, 0, 0}, 0x7FF}, []byte{0, 0x1F, 0xFC, 0x00}},
+		// set 11 zeros in a row
 		{"c", args{1, 11, []byte{0xFF, 0xFF, 0xFF, 0xFF}, 0}, []byte{0xFF, 0xE0, 0x03, 0xFF}},
+		// set at the left
+		{"d", args{0, 11, []byte{0, 0, 0, 0}, 0x7FF}, []byte{0xFF, 0xE0, 0, 0}},
+		// set at the right
+		{"e", args{7, 11, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0x7FF},
+			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0x07, 0xFF}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -267,9 +297,13 @@ func Test_crc8(t *testing.T) {
 		b    []byte
 		want byte
 	}{
+		// crc of 0 is 0
 		{"a", []byte{0}, 0},
+		// crc of 1 is 0x1D (the polynomial for the 8 bit crc)
 		{"b", []byte{1}, 0x1D},
+		// simple one
 		{"c", []byte{1, 2, 3, 4}, 62},
+		// show that if we make a tiny change the crc changes a lot
 		{"d", []byte("This is a test"), 214},
 		{"e", []byte("this is a test"), 59},
 	}
@@ -292,6 +326,8 @@ func Test_nextMultipleOf(t *testing.T) {
 		args args
 		want int
 	}{
+		// this function just gives us the next multiple of a particular value;
+		// test some common occurrences in our system
 		{"a", args{8, 11}, 11},
 		{"b", args{24, 11}, 33},
 		{"c", args{88, 11}, 88},
