66580d084fdd8e8f1b2d9f523f3cebf39d00b7ed
Kent Quirk <kentquirk@gmail.com>
2018-08-01 16:25:52 -0400

    Create bitset256 and tests

---
+++ pkg/bitset256/bitset256.go
@@ -0,0 +1,149 @@
+package bitset256
+
+import (
+	"encoding/hex"
+	"errors"
+	"math/bits"
+)
+
+// Bitset256 is an efficient way to store individual bits corresponding to 256 values (i.e.,
+// using a byte as an index). The bits are stored in an array of 4 64-bit words, in little-endian
+// word order (the 0 bit is the 0 bit of the 0th word).
+type Bitset256 [4]uint64
+
+// New creates a new bitset and allows setting some of its bits at the same time
+func New(ixs ...int) *Bitset256 {
+	b := &Bitset256{}
+	for _, i := range ixs {
+		b.Set(i)
+	}
+	return b
+}
+
+// Clone creates a copy of a bitset
+func (b *Bitset256) Clone() *Bitset256 {
+	c := *b
+	return &c
+}
+
+// Get retrieves the value of a single bit at the given index.
+// The index is taken mod256.
+func (b *Bitset256) Get(ix int) bool {
+	w := (ix & 0xFF) / 64
+	mask := uint64(1) << byte(ix%64)
+	return (b[w] & mask) != 0
+}
+
+// Set unconditionally forces a single bit at the index to 1 and returns the pointer to the bitset
+// The index is taken mod256.
+func (b *Bitset256) Set(ix int) *Bitset256 {
+	w := (ix & 0xFF) / 64
+	// create a mask for the bit we need to set
+	mask := uint64(1) << byte(ix%64)
+	b[w] |= mask
+	return b
+}
+
+// Clear unconditionally forces a single bit to 0 and returns the pointer to the bitset
+// The index is taken mod256.
+func (b *Bitset256) Clear(ix int) *Bitset256 {
+	w := (ix & 0xFF) / 64
+	// this mask needs all bits on except the one we're clearing
+	mask := ^(uint64(1) << byte(ix%64))
+	b[w] &= mask
+	return b
+}
+
+// Equals returns true if the two bitsets have identical contents
+func (b *Bitset256) Equals(other *Bitset256) bool {
+	for i := 0; i < 4; i++ {
+		if b[i] != other[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// Intersect returns a pointer to a new Bitset256 that is the intersection
+// of its two source bitsets (the only bits that are set are the ones where
+// both source sets had a 1 bit)
+func (b *Bitset256) Intersect(other *Bitset256) *Bitset256 {
+	r := b.Clone()
+	for i := 0; i < 4; i++ {
+		r[i] &= other[i]
+	}
+	return r
+}
+
+// Union returns a pointer to a new Bitset256 that is the union
+// of its two source bitsets (the only bits that are set are the ones where
+// either source set had a 1 bit)
+func (b *Bitset256) Union(other *Bitset256) *Bitset256 {
+	r := b.Clone()
+	for i := 0; i < 4; i++ {
+		r[i] |= other[i]
+	}
+	return r
+}
+
+// IsSubsetOf returns true if all of the bits in a bitset are also in the other bitset.
+func (b *Bitset256) IsSubsetOf(other *Bitset256) bool {
+	return b.Equals(b.Intersect(other))
+}
+
+// Count returns the number of 1 bits that are set
+func (b *Bitset256) Count() int {
+	c := 0
+	for i := 0; i < 4; i++ {
+		c += bits.OnesCount64(b[i])
+	}
+	return c
+}
+
+// AsBytes returns the bitset as a slice of 32 bytes, where the 0 bits in the bitset are in the
+// last element of the slice. This is so rendering the slice to a visual form will show the
+// bits in the correct order.
+func (b *Bitset256) AsBytes() []byte {
+	ba := make([]byte, 32)
+	for i := uint(0); i < 4; i++ {
+		for j := uint(0); j < 8; j++ {
+			ba[j*4+i] = byte((b[3-i] >> ((7 - j) * 8)) & 0xFF)
+		}
+	}
+	return ba
+}
+
+// FromBytes takes a slice of 32 bytes and builds a Bitset256 from it, following
+// the same rules as AsBytes (the last byte in the slice corresponds to the zeroth bits).
+func FromBytes(ba []byte) (*Bitset256, error) {
+	if len(ba) != 32 {
+		return nil, errors.New("wrong number of bytes")
+	}
+	b := New()
+	for i := uint(0); i < 4; i++ {
+		for j := uint(0); j < 8; j++ {
+			b[3-i] |= uint64(ba[j*4+i]) << ((7 - j) * 8)
+		}
+	}
+	return b, nil
+}
+
+// AsHex returns a string representation of the bitset as a 256-bit number in hex (64 characters)
+// It follows the same rules as AsBytes.
+func (b *Bitset256) AsHex() string {
+	return hex.EncodeToString(b.AsBytes())
+}
+
+// FromHex builds a Bitset256 from a hex string like the one AsHex generates.
+func FromHex(s string) (*Bitset256, error) {
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		return nil, err
+	}
+	return FromBytes(b)
+}
+
+// String implements Stringer for Bitset256.
+func (b *Bitset256) String() string {
+	return b.AsHex()
+}
---
+++ pkg/bitset256/bitset256_test.go
@@ -0,0 +1,133 @@
+package bitset256
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmpty(t *testing.T) {
+	b := New()
+	assert.False(t, b.Get(5))
+	assert.Equal(t, 0, b.Count())
+	assert.Equal(t, "0000000000000000000000000000000000000000000000000000000000000000", b.AsHex())
+}
+
+func TestSimple(t *testing.T) {
+	b := New()
+	assert.False(t, b.Get(1))
+	c := b.Set(1)
+	assert.Equal(t, b, c)
+	assert.True(t, b.Get(1))
+	fmt.Println(b)
+	b.Clear(1)
+	fmt.Println(b)
+	assert.False(t, b.Get(1))
+	assert.False(t, b.Get(2))
+}
+
+func TestClone(t *testing.T) {
+	b := New().Set(1).Set(35)
+	assert.Equal(t, 2, b.Count())
+	c := b.Clone()
+	assert.Equal(t, 2, c.Count())
+	assert.Equal(t, b, c)
+	c.Set(28)
+	assert.Equal(t, 3, c.Count())
+	assert.Equal(t, 2, b.Count())
+}
+
+func TestAllBits(t *testing.T) {
+	b := New()
+	for i := 0; i < 256; i++ {
+		assert.False(t, b.Get(i))
+		b.Set(i)
+		assert.True(t, b.Get(i))
+		b.Clear(i)
+		assert.False(t, b.Get(i))
+		b.Set(i)
+		assert.Equal(t, int(i+1), b.Count())
+	}
+}
+
+func setMultiples(n int) *Bitset256 {
+	b := New()
+	for i := 0; i < 256; i += n {
+		b.Set(i)
+	}
+	return b
+}
+
+func TestAsBytes(t *testing.T) {
+	b := setMultiples(7)
+	assert.Equal(t, 37, b.Count())
+	ba := b.AsBytes()
+	c, err := FromBytes(ba)
+	assert.Nil(t, err)
+	assert.Equal(t, b, c)
+	d, err := FromBytes(ba[:len(ba)-1])
+	assert.NotNil(t, err)
+	assert.Nil(t, d)
+}
+
+func TestAsHex(t *testing.T) {
+	b := setMultiples(7)
+	assert.Equal(t, 37, b.Count())
+	s := b.AsHex()
+	c, err := FromHex(s)
+	assert.Nil(t, err)
+	assert.Equal(t, b, c)
+	d, err := FromHex(s[:len(s)-1])
+	assert.NotNil(t, err)
+	assert.Nil(t, d)
+}
+
+func TestSubset(t *testing.T) {
+	b := New().Set(1).Set(2).Set(3)
+	c := New().Set(1).Set(2)
+	assert.Equal(t, 3, b.Count())
+	assert.Equal(t, 2, c.Count())
+	assert.True(t, c.IsSubsetOf(b))
+	assert.False(t, b.IsSubsetOf(c))
+	assert.True(t, c.IsSubsetOf(c))
+	assert.True(t, b.IsSubsetOf(b))
+}
+
+func TestNewMulti(t *testing.T) {
+	b := New(1, 2, 3)
+	c := New(1, 2)
+	assert.Equal(t, 3, b.Count())
+	assert.Equal(t, 2, c.Count())
+	assert.True(t, c.IsSubsetOf(b))
+	assert.False(t, b.IsSubsetOf(c))
+	assert.True(t, c.IsSubsetOf(c))
+	assert.True(t, b.IsSubsetOf(b))
+}
+
+func TestIntersect(t *testing.T) {
+	fizz := setMultiples(3)
+	buzz := setMultiples(5)
+	fizzbuzz := fizz.Intersect(buzz)
+	assert.Equal(t, 18, fizzbuzz.Count())
+	all := fizz.Union(buzz)
+	assert.Equal(t, 120, all.Count())
+	assert.True(t, fizzbuzz.IsSubsetOf(fizz))
+	assert.True(t, fizzbuzz.IsSubsetOf(buzz))
+	assert.True(t, fizzbuzz.IsSubsetOf(fizzbuzz))
+	assert.True(t, fizzbuzz.IsSubsetOf(all))
+}
+
+func TestUnion(t *testing.T) {
+	fizz := setMultiples(3)
+	assert.Equal(t, 256/3+1, fizz.Count())
+	buzz := setMultiples(5)
+	assert.Equal(t, 256/5+1, buzz.Count())
+	all := fizz.Union(buzz)
+	assert.Equal(t, 256/3+1, fizz.Count())
+	assert.Equal(t, 256/5+1, buzz.Count())
+	assert.Equal(t, 120, all.Count())
+	assert.True(t, fizz.IsSubsetOf(all))
+	assert.True(t, buzz.IsSubsetOf(all))
+	assert.True(t, all.IsSubsetOf(all))
+}
