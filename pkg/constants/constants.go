f60ba444e2f11d44bb23ef9e45c2c10827e35779
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-10-09 10:30:50 +0200

    add RateDenominator constant

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -6,47 +6,65 @@ import (
 	"time"
 )
 
-// CurrencyName is the official name of the currency.
-const CurrencyName = "ndau"
+const (
+	// CurrencyName is the official name of the currency.
+	CurrencyName = "ndau"
 
-// CurrencyQuantum is the official name of the smallest possible
-// unit of the currency.
-const CurrencyQuantum = "napu"
+	// CurrencyQuantum is the official name of the smallest possible
+	// unit of the currency.
+	CurrencyQuantum = "napu"
 
-// QuantaPerUnit is the number of quanta in a single unit of
-// ndau.
-const QuantaPerUnit = 100000000
+	// QuantaPerUnit is the number of quanta in a single unit of
+	// ndau.
+	QuantaPerUnit = 100000000
 
-// MaxQuantaPerAddress is the number of quanta that
-// can be tracked in a single address.
-const MaxQuantaPerAddress = math.MaxInt64
+	// NapuPerNdau is a more human-friendly synonym of QuantaPerUnit
+	NapuPerNdau = QuantaPerUnit
 
-// TimestampFormat is the format string used to parse timestamps.
-const TimestampFormat = "2006-01-02T15:04:05Z"
+	// RateDenominator is the implied denominator for interest rates.
+	//
+	// EAI rates are expressed as integers, and integer math is performed
+	// for determinism. However, the interest-like calculations of EAI
+	// expect non-integer rates. We square this circle by treating them
+	// as rationals using an implied denominator, specified here.
+	//
+	// It is coincidence, not required for correct functioning, that the
+	// rate denominator is equal to QuantaPerUnit.
+	RateDenominator = QuantaPerUnit
 
-// EpochStart is the text representation of the start time of our Epoch.
-const EpochStart = "2000-01-01T00:00:00Z"
+	// MaxQuantaPerAddress is the number of quanta that
+	// can be tracked in a single address.
+	MaxQuantaPerAddress = math.MaxInt64
 
-// Epoch is the basic moment from which Ndau chain time calculations begin.
-var Epoch time.Time
+	// TimestampFormat is the format string used to parse timestamps.
+	TimestampFormat = "2006-01-02T15:04:05Z"
 
-// MaxTimestamp is the maximum value a timestamp can take on
-const MaxTimestamp = math.MaxInt64
+	// EpochStart is the text representation of the start time of our Epoch.
+	EpochStart = "2000-01-01T00:00:00Z"
 
-// MinTimestamp is the minimum value a timestamp can take on
-const MinTimestamp = 0
+	// MaxTimestamp is the maximum value a timestamp can take on
+	MaxTimestamp = math.MaxInt64
 
-// DurationFormat is the format regex used to parse durations
-const DurationFormat = `(?i)^p?((?P<years>\d+)y)?((?P<months>\d{1,2})m)?((?P<days>\d{1,2})d)?(t((?P<hours>\d{1,2})h)?((?P<minutes>\d{1,2})m)?((?P<seconds>\d{1,2})s)?((?P<micros>\d{1,6})[μu]s?)?)?$`
+	// MinTimestamp is the minimum value a timestamp can take on
+	MinTimestamp = 0
 
-// DurationRE is the regular expression used to parse Durations
-var DurationRE *regexp.Regexp
+	// DurationFormat is the format regex used to parse durations
+	DurationFormat = `(?i)^p?((?P<years>\d+)y)?((?P<months>\d{1,2})m)?((?P<days>\d{1,2})d)?(t((?P<hours>\d{1,2})h)?((?P<minutes>\d{1,2})m)?((?P<seconds>\d{1,2})s)?((?P<micros>\d{1,6})[μu]s?)?)?$`
 
-// MaxDuration is the maximum value a duration can contain
-const MaxDuration = math.MaxInt64
+	// MaxDuration is the maximum value a duration can contain
+	MaxDuration = math.MaxInt64
 
-// MinDuration is the minimum value a duration can contain
-const MinDuration = math.MinInt64
+	// MinDuration is the minimum value a duration can contain
+	MinDuration = math.MinInt64
+)
+
+var (
+	// Epoch is the basic moment from which Ndau chain time calculations begin.
+	Epoch time.Time
+
+	// DurationRE is the regular expression used to parse Durations
+	DurationRE *regexp.Regexp
+)
 
 func init() {
 	Epoch, _ = time.Parse(TimestampFormat, EpochStart)
