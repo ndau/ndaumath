bd690b92b76ac4279aebaaa08125ea8eaff64133
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-10-16 13:11:50 +0200

    remove unused private function

--- pkg/key/extendedkey.go
+++ pkg/key/extendedkey.go
@@ -392,16 +392,6 @@ func (k *ExtendedKey) ECPrivKey() (*btcec.PrivateKey, error) {
 	return privKey, nil
 }
 
-// paddedAppend appends the src byte slice to dst, returning the new slice.
-// If the length of the source is smaller than the passed size, leading zero
-// bytes are appended to the dst slice before appending src.
-func paddedAppend(size uint, dst, src []byte) []byte {
-	for i := 0; i < int(size)-len(src); i++ {
-		dst = append(dst, 0)
-	}
-	return append(dst, src...)
-}
-
 const extraLen = 1 + 3 + 4 + 32
 
 // extra serializes all extra data associated with this key
