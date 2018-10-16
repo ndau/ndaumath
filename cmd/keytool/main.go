76e76a703f921eda76d36de9589a6fa00acefc61
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-10-16 16:05:57 +0200

    Add keytool to assist in generating and manipulating hd keys
    All the work right now is under the hd subcommand, in the hope that
    eventually we'll flesh the tool out, but for now it does what
    we need it for.

---
+++ cmd/keytool/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	cli "github.com/jawher/mow.cli"
+)
+
+func main() {
+	app := cli.App("keytool", "manipulate key strings on the command line")
+
+	app.Command("hd", "manipulate HD keys", hd)
+
+	app.Run(os.Args)
+}
+
+// hd subcommand
+func hd(cmd *cli.Cmd) {
+	cmd.Command("new", "create a new HD key", cmdHDNew)
+	cmd.Command("public", "create a public key from supplied key", cmdHDPublic)
+	cmd.Command("child", "create a child key derived from the supplied key", cmdHDChild)
+}
