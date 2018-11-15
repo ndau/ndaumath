fbb2a86b6883cc19e5416f716950b3f5fdceaf6d
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-11-15 17:28:18 +0100

    add long-form for ed25519 subcommand name

--- cmd/keytool/main.go
+++ cmd/keytool/main.go
@@ -10,7 +10,7 @@ func main() {
 	app := cli.App("keytool", "manipulate key strings on the command line")
 
 	app.Command("hd", "manipulate HD keys", hd)
-	app.Command("ed", "manipulate ed25519 keys", ed)
+	app.Command("ed ed25519", "manipulate ed25519 keys", ed)
 	app.Command("sign", "sign some data", cmdSign)
 	app.Command("verify", "verify some data", cmdVerify)
 	app.Command("addr", "generate addresses from public keys", cmdAddr)
