1d1591c72b1538cfda13e62f4e7a5e9fd80c5fda
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-03-14 17:33:32 +0100

    WIP: Add phase 2 and 3 integer calculations to pricecurve
    The errors aren't in as tight a tolerance as we'd really like,
    but we can work out how to fix that later.

--- pkg/pricecurve/pricecurve_test.go
+++ pkg/pricecurve/pricecurve_test.go
@@ -1,13 +1,19 @@
 package pricecurve
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
 	"testing"
 
 	"github.com/oneiro-ndev/ndaumath/pkg/constants"
 	"github.com/oneiro-ndev/ndaumath/pkg/types"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_PriceAtUnit(t *testing.T) {
+func Test_ApproxPriceAtUnit(t *testing.T) {
 	tests := []struct {
 		name       string
 		nunitsSold types.Ndau
@@ -22,7 +28,7 @@ func Test_PriceAtUnit(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := PriceAtUnit(tt.nunitsSold * constants.QuantaPerUnit); got != tt.want {
+			if got := ApproxPriceAtUnit(tt.nunitsSold * constants.QuantaPerUnit); got != tt.want {
 				t.Errorf("PriceAtUnit() = %v, want %v", got, tt.want)
 			}
 		})
@@ -43,7 +49,7 @@ func Test_UnitAtPrice(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := UnitAtPrice(tt.price); got != tt.want {
+			if got := ApproxUnitAtPrice(tt.price); got != tt.want {
 				t.Errorf("UnitAtPrice() = %v, want %v", got, tt.want)
 			}
 		})
@@ -67,9 +73,100 @@ func TestTotalPriceFor(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := TotalPriceFor(tt.args.numNdau, tt.args.alreadySold); got != tt.want {
+			if got := ApproxTotalPriceFor(tt.args.numNdau, tt.args.alreadySold); got != tt.want {
 				t.Errorf("TotalPriceFor() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_phase1_increases_monotonically(t *testing.T) {
+	var prev Nanocent
+	var curr Nanocent
+	for i := 0; i < 10000; i++ {
+		curr = phase1(uint64(i))
+		if curr <= prev {
+			t.Log("block:", i)
+			t.Log("curr: ", curr)
+			t.Log("prev: ", prev)
+		}
+		require.True(t, curr > prev)
+		prev = curr
+	}
+}
+
+func TestPhase1(t *testing.T) {
+	var dataOut io.Writer
+
+	if true { // probably disable this later sometime
+		f, err := os.Create("phase1errors.csv")
+		require.NoError(t, err)
+		defer f.Close()
+		dataOut = bufio.NewWriter(f)
+		fmt.Fprintln(dataOut, "block,using floats,using ints,epsilon")
+	}
+
+	for block := uint64(0); block < 10000; block++ {
+		sold := block * constants.QuantaPerUnit * saleBlockQty
+		apau := ApproxPriceAtUnit(types.Ndau(sold))
+		pau := phase1(block)
+		paud := float64(pau) / float64(dollar)
+
+		epsilon := (apau - paud) / apau
+
+		if dataOut != nil {
+			fmt.Fprintf(dataOut, "%d,%f,%f,%f\n", block, apau, paud, epsilon)
+		}
+
+		t.Run(fmt.Sprint(block), func(t *testing.T) {
+			require.True(t, math.Abs(epsilon) < 0.000001, "abs epsilon must be < 0.0000001")
+		})
+	}
+}
+
+func Test_phase23IncreasesMonotonically(t *testing.T) {
+	var prev Nanocent
+	var curr Nanocent
+	var err error
+	for i := 10000; i < 30000; i++ {
+		curr, err = phase23(int64(i))
+		require.NoError(t, err)
+		if curr <= prev {
+			t.Log("block:", i)
+			t.Log("curr: ", curr)
+			t.Log("prev: ", prev)
+		}
+		require.True(t, curr > prev)
+		prev = curr
+	}
+}
+
+func TestPhase23(t *testing.T) {
+	var dataOut io.Writer
+
+	if true { // probably disable this later sometime
+		f, err := os.Create("phase23errors.csv")
+		require.NoError(t, err)
+		defer f.Close()
+		dataOut = bufio.NewWriter(f)
+		fmt.Fprintln(dataOut, "block,using floats,using ints,epsilon")
+	}
+
+	for block := int64(10000); block < 30000; block++ {
+		sold := block * constants.QuantaPerUnit * saleBlockQty
+		apau := ApproxPriceAtUnit(types.Ndau(sold))
+		pau, err := phase23(block)
+		require.NoError(t, err)
+		paud := float64(pau) / float64(dollar)
+
+		epsilon := (apau - paud) / apau
+
+		if dataOut != nil {
+			fmt.Fprintf(dataOut, "%d,%f,%f,%f\n", block, apau, paud, epsilon)
+		}
+
+		t.Run(fmt.Sprint(block), func(t *testing.T) {
+			require.True(t, math.Abs(epsilon) < 0.000001, "abs epsilon must be < 0.0000001")
 		})
 	}
 }
