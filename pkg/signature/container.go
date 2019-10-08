6386f121ced49daaabf14e60f22fb00f59cc71e2
Kent Quirk <kentquirk@gmail.com>
2019-10-08 16:37:07 -0400

    Add copyright and license

--- pkg/signature/container.go
+++ pkg/signature/container.go
@@ -2,6 +2,15 @@ package signature
 
 //go:generate msgp
 
+// ----- ---- --- -- -
+// Copyright 2019 Oneiro NA, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License 2.0 (the "License").  You may not use
+// this file except in compliance with the License.  You can obtain a copy
+// in the file LICENSE in the source distribution or at
+// https://www.apache.org/licenses/LICENSE-2.0.txt
+// - -- --- ---- -----
+
 //msgp:tuple IdentifiedData
 
 // AlgorithmID is an identifier uniquely associated with each supported signature algorithm
