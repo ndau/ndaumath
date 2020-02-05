c177ce4af041592d6154ec3f7b15337739fe1a26
Ed McNierney <ed@mcnierney.com>
2020-02-05 16:14:24 -0500

    Correct/add copyright messages

--- cmd/keyaddr/wasm_exec.js
+++ cmd/keyaddr/wasm_exec.js
@@ -1,3 +1,13 @@
+/* ----- ---- --- -- -
+ * Copyright 2020 The Axiom Foundation. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.apache.org/licenses/LICENSE-2.0.txt
+ * - -- --- ---- -----
+ */
+
 // Copyright 2018 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
--- pkg/key/extendedkey.go
+++ pkg/key/extendedkey.go
@@ -1,11 +1,12 @@
 // ----- ---- --- -- -
-// Copyright 2018, 2019 Oneiro NA, Inc. All Rights Reserved.
+// Copyright 2018, 2019, 2020 The Axiom Foundation. All Rights Reserved.
 //
 // Licensed under the Apache License 2.0 (the "License").  You may not use
 // this file except in compliance with the License.  You can obtain a copy
 // in the file LICENSE in the source distribution or at
 // https://www.apache.org/licenses/LICENSE-2.0.txt
 // - -- --- ---- -----
+
 
 // Copyright (c) 2014-2016 The btcsuite developers
 
